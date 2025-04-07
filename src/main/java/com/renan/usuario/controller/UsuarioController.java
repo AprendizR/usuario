d96a868d8db629e037b111b25e6180073c6ff154
Renan Rosa <renanrosa1005@gmail.com>
2025-04-07 16:27:51 -0300

    Exclusão de classe de testes

--- src/main/java/com/renan/usuario/controller/UsuarioController.java
+++ src/main/java/com/renan/usuario/controller/UsuarioController.java
@@ -74,5 +74,4 @@ public class UsuarioController {
                                                         @RequestHeader ("authorization") String token){
         return ResponseEntity.ok(usuarioService.cadastroTelefone(token, dto));
     }
-
 }
